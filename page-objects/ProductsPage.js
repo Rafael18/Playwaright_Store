3033bef97efdd1cd84d73a65cc8664a0d0d7067a
Rafael Batista <rafaeltec_@live.com>
2025-07-29 22:54:52 -0300

    Inclusão de intens no carrinho, remoação de 1 item do carrinho, ordenação dos itens, etc

---
+++ page-objects/ProductsPage.js
@@ -0,0 +1,44 @@
+import { expect } from "@playwright/test"
+import { Navigation } from './Navigation'
+
+export class ProductsPage {
+
+    constructor(page) {
+        this.page = page
+
+        this.addButtons = page.locator('[data-qa="product-button"]')
+        this.sortDropdown = page.locator('[data-qa="sort-dropdown"]')
+        this.productTitle = page.locator('[data-qa="product-title"]')
+    }
+
+    visit = async () => {
+        await this.page.goto("/")
+    }
+
+    addProductToBasket = async (index) => {
+        const specificAddButton = this.addButtons.nth(index)
+        await specificAddButton.waitFor()
+        await expect(specificAddButton).toHaveText('Add to Basket')
+
+        const _navigation = new Navigation(this.page)
+
+        const basketCountBeforeAdding = await _navigation.getBasketCount()
+        await specificAddButton.click()
+        await expect(specificAddButton).toHaveText('Remove from Basket')
+        const basketCountAfterAdding =  await _navigation.getBasketCount()
+
+        expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding)
+    }
+
+    sortByCheapest = async () => {
+        await this.sortDropdown.waitFor()
+        // get order of products
+        await this.productTitle.first().waitFor()
+        const productTitleBeforeSorting = await this.productTitle.allInnerTexts()
+        await this.sortDropdown.selectOption("price-asc")
+        const productTitleAfterSorting = await this.productTitle.allInnerTexts()
+
+        expect(productTitleAfterSorting).not.toEqual(productTitleBeforeSorting)
+        await this.page.pause()
+    }
+}
---
+++ tests/new_user_full_journey.spec.js
@@ -0,0 +1,21 @@
+import { test } from '@playwright/test'
+import { ProductsPage } from './../page-objects/ProductsPage'
+import { Navigation } from './../page-objects/Navigation'
+import { Checkout } from './../page-objects/Checkout'
+
+test.only('New user full end-to-end test journey', async ({ page }) => {
+    const productPage = new ProductsPage(page)
+
+    await productPage.visit()
+    await productPage.sortByCheapest()
+    await productPage.addProductToBasket(0)
+    await productPage.addProductToBasket(1)
+    await productPage.addProductToBasket(2)
+
+    const _navigation = new Navigation(page)
+    await _navigation.goToCheckout()
+
+    const _checkout = new Checkout(page)
+    await _checkout.removeCheapestProduct()
+    // await page.pause()
+})
